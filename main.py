d1d6e28596084375aedf04e900a1021573736832
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-04-27 13:35:04 +0200

    added stuff to main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
 import imputation
 import cleansing
+import feat_selection
 
 
 def main():
@@ -26,11 +27,26 @@ def main():
     valid = pd.DataFrame(valid)
     test = pd.DataFrame(test)
 
-    plt.imshow(train.corr())
-    plt.colorbar()
-    plt.show()
+    # plt.imshow(train.corr())
+    # plt.colorbar()
+    # plt.show()
 
     imputation.imputation(train)
+
+    # plt.imshow(feat_selection.calc_MI_matrix(train))
+    # plt.colorbar()
+    # plt.show()
+
+    features = df.columns.to_numpy().tolist()
+    features_close = feat_selection.remove_similar_features(train, 'Vote', 10)
+    print(f'features after close removal: {features_close}')
+    features_far = feat_selection.remove_far_features(train, 'Vote', 10)
+    print(f'features after far removal: {features_far}')
+    features_close_far = feat_selection.remove_similar_features(train[features_close], 'Vote', 10)
+    print(f'features after close and then far removal: {features_close_far}')
+    features_far_close = feat_selection.remove_far_features(train[features_far], 'Vote', 10)
+    print(f'features after far removal: {features_far_close}')
+
     train = cleansing.change_categorials(train)
     train.to_csv("hello.csv")
 
