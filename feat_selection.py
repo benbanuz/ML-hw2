d5ca0ff336f559db0b3192f99409d2124154fe76
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-05-01 09:50:04 +0200

    implemented relief algorithm

--- feat_selection.py
+++ feat_selection.py
@@ -107,6 +107,37 @@ def remove_far_features(df: pd.DataFrame, target: str, max_remove: int, mi_thres
     return [target] + list(tuple(zip(*sorted_mi_vals))[0])
 
 
+def closest_row(mat: np.ndarray, row: np.ndarray) -> int:
+    dists: np.ndarray = np.sum(np.power((mat - row), 2), axis=1)
+    return np.argmin(dists)
+
+
+def relief(df: pd.DataFrame, target: str, max_remove: int, num_iter=10) -> List[str]:
+    features = list(set(df.columns.to_numpy().tolist()).difference({target}))
+    weights = np.zeros(len(features))
+
+    for row_idx in np.random.randint(0, len(df.index), num_iter):
+        target_val = df[target].iloc[row_idx]
+        hit_indices = df[target] == target_val
+
+        val = df[features].iloc[row_idx].values
+
+        misses = df[~hit_indices][features].values
+
+        hit_indices[row_idx] = False
+        hits = df[hit_indices][features].values
+
+        near_hit = df[features].iloc[closest_row(hits, val)]
+        near_miss = df[features].iloc[closest_row(misses, val)]
+
+        weights += np.power((val - near_miss), 2) - np.power((val - near_hit), 2)
+
+    feat_weights = {feat: weights[i] for i, feat in enumerate(features)}
+    sorted_feats = dict(sorted(feat_weights.items(), key=lambda kv: (kv[1], kv[0])))
+
+    return list(sorted_feats.keys())[max_remove:]
+
+
 def wrapper_SFS(df_train: pd.DataFrame, df_valid: pd.DataFrame, target: str, clf, cur_score: float = -np.inf,
                 features: List[str] = [], max_features=17) -> List[str]:
     if len(features) == max_features:
@@ -132,7 +163,6 @@ def wrapper_SFS(df_train: pd.DataFrame, df_valid: pd.DataFrame, target: str, clf
 
 def wrapper_SBS(df_train: pd.DataFrame, df_valid: pd.DataFrame, target: str, clf, cur_score: float = -np.inf,
                 features: List[str] = None, max_features=17) -> List[str]:
-
     if features is None:
         features = df_train.columns.to_numpy().tolist()
         features.remove(target)
