6d6301313141a6c4124e5d4a9a46d70b5f589c0c
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-05-01 09:51:00 +0200

    made the train/valid/test split stratified

--- loading.py
+++ loading.py
@@ -7,12 +7,12 @@ def load_csv():
     return data
 
 
-def split_data(data: pd.DataFrame, valid_ratio: float = 0.15, test_ratio: float = 0.1):
+def split_data(data: pd.DataFrame, target: str, valid_ratio: float = 0.15, test_ratio: float = 0.1):
     train_ratio = 1 - (valid_ratio + test_ratio)
     assert train_ratio >= 0.5
 
-    train, test = train_test_split(data, test_size=test_ratio)
+    train, test = train_test_split(data, test_size=test_ratio, stratify=data[target])
     valid_ratio = valid_ratio / (1 - test_ratio)
-    train, valid = train_test_split(train, test_size=valid_ratio)
+    train, valid = train_test_split(train, test_size=valid_ratio, stratify=train[target])
 
     return train, valid, test
